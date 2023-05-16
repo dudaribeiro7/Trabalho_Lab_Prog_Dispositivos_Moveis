9a10fb455c87c9aa2473938962cd6e259604b5d0
Christian Rafael <christianrafael102@gmail.com>
2023-05-16 10:24:45 -0300

    teste

--- fixIt/App.js
+++ fixIt/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>FixIt!</Text>
+      <Text>FixIt!!!</Text>
       <StatusBar style="auto" />
     </View>
   );
